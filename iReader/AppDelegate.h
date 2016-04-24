b10ed8d786b322f95399b575c9bcb7269240bef0
七度雨 <269656131@qq.com>
2016-04-24 10:33:59 -0700

    Initial Commit

---
+++ iReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iReader
+//
+//  Created by SevenCelsius on 16/4/24.
+//  Copyright © 2016年 SevenCelsius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iReader
+//
+//  Created by SevenCelsius on 16/4/24.
+//  Copyright © 2016年 SevenCelsius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
